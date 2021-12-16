b153c6bf0906160d15466bceff7ba56dfe3535df
Nakrani <ashish.nakrani.60@gmail.com>
2021-12-16 15:36:34 +0530

    create full animation site

---
+++ js/script.js
@@ -0,0 +1,124 @@
+let globalVolume = 0;
+let isVolumeOn = false;
+$("a").on('mouseover', function(e) {
+  if (!isVolumeOn) {
+    return false;
+  }
+  var x = document.getElementById("bg_music");
+  x.volume = globalVolume;
+  x.play();
+});
+
+$(document).ready(function() {
+  $('#music_btn').on('click', function() {
+    isVolumeOn = !isVolumeOn;
+    if(isVolumeOn) {
+      globalVolume = 0.05;
+      $(this).html('On');
+    } else {
+      globalVolume = 0;
+      $(this).html('Off');
+    }
+  });
+  setTimeout(function () {
+    $('header').trigger('click');
+  }, 500)
+   
+    $('.loading-overlay').show();
+    setTimeout(function() {$('.loading-overlay').hide()}, 5000);
+
+    var str_section = window.location.href;
+    var section_id = str_section.substring(str_section.indexOf('#') + 1, str_section.length);
+    if (section_id == "Token_allocation") {
+        window.setTimeout(initHighChart, 1000);
+    }
+});
+
+
+// FAQ start
+var MODULE = MODULE || {};
+
+(function($, MODULE) {
+  MODULE.init_accordion = function() {
+    // Logic for accordion
+    var accordion = (function() {
+      var $accordion_elem = $(".c-accordion"),
+        $accordion_headerlink = $accordion_elem.find(
+          ".js-accordion__entry-header-link"
+        ),
+        $accordion_item = $accordion_elem.find(".c-accordion__entry");
+
+      var settings = {
+        speed: parseInt($accordion_elem.attr("data-speed")) || 400,
+        individual_openable: $accordion_elem.attr("data-individual-openable") === "true"
+      };
+
+      return {
+        init: function() {
+          $accordion_headerlink.on("click", function(e) {
+            e.preventDefault();
+            accordion.toggle($(this));
+          });
+
+          if (
+            !settings.individual_openable &&
+            $(".c-accordion__entry.is-expanded").length > 1
+          ) {
+            $(".c-accordion__entry.is-expanded").removeClass(
+              "is-expanded"
+            );
+          }
+        },
+        toggle: function(self) {
+          if (
+            !settings.individual_openable &&
+            self[0] !=
+              self
+                .closest(".c-accordion")
+                .find(
+                  ".c-accordion__entry.is-expanded .c-accordion__entry-header-link"
+                )[0]
+          ) {
+            self
+              .closest(".c-accordion")
+              .find(".c-accordion__entry")
+              .removeClass("is-expanded")
+              .find(".c-accordion__entry-body")
+              .slideUp();
+          }
+
+          self
+            .closest(".c-accordion__entry")
+            .toggleClass("is-expanded")
+            .attr("aria-expanded", function(i, attr) {
+              return attr == "true" ? "false" : "true";
+            });
+
+          self.parent().attr("aria-selected", function(i, attr) {
+            return attr == "true" ? "false" : "true";
+          });
+
+          self
+            .parent()
+            .next()
+            .stop()
+            .slideToggle(settings.speed)
+            .attr("aria-hidden", function(i, attr) {
+              return attr == "true" ? "false" : "true";
+            });
+        }
+      };
+    })();
+
+    accordion.init();
+  };
+
+  /* READY FUNCTION
+  ====================== */
+
+  $(function() {
+    MODULE.init_accordion();
+  });
+})(jQuery, MODULE);
+
+// FAQ ends
